980da0fa11273c50fda1f719574c04b168b54d96
Anna Chirapongse <achirapongse@gmail.com>
2020-10-05 20:05:17 -0400

    Updating the three files

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # weather-dashboard
 // Creating a weather app where users can search the cities to see how current weather conditions.
+// Adding the jQuery and ajax call for to the javascript for the api call for the current weather, uv and five day forcast.
+//Set up the html framework for the elements to be populated from the Open weather Api.
--- script.js
+++ script.js
@@ -1,12 +1,45 @@
-$("submit").on("click", function(){
-    var city = $(this).attr("data-city");
-    var queryURL = api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid={API key}
-})
+$("#submit").on("click", function(event){
+    event.preventDefault();
+    var city = $("#inputCity").val();
+    // console.log(city);
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+city+ "&appid=ffa2450d91a6f2a29c9510166ab57af7"
+    // console.log(queryURL)
 
-$.ajx({
+$.ajax({
     url: queryURL,
     method: "GET"
-})
+    })
+    
+    .then(function(response){
+        $("#city").text(JSON.stringify(response));
+        var tBody = $("tbody");
+        var tRow = $("<tr>");
+        var tempTd = $("<td>").text(response.main.temp);
+        var humidityTd = $("<td>").text(response.main.huidity);
+        var windSpeedTd = $("<td>").text(response.wind.speed);
+        tRow.append(temp, humidity,speed);
+        tBody.append(tRow);
+        
+        var lat = response.coord.lat
+        var lon = response.coord.lon
+        var uvURL = "https://api.openweathermap.org/data/2.5/uvi?lat="+lat+"&lon="+lon+ "&appid=ffa2450d91a6f2a29c9510166ab57af7"
+        return $.ajax({
+            url: uvURL,
+            method: "GET"
+        })
+        .then(function(uvreponse){
+            //console.log(uvreponse)
+            var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+ "&appid=ffa2450d91a6f2a29c9510166ab57af7"
+            return $.ajax ({
+                url: forecastUrl,
+                method: "GET"
+            })
+            .then(function(forecastresponse){
+                console.log(forecastresponse)
+            })
+        })
 
-.then(function(response)
-)
+    
+
+    })
+})
