75493f40727c0362309d32507234da6f0a0a6668
Anna Chirapongse <achirapongse@gmail.com>
2020-10-07 22:10:36 -0400

    Last Updates

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 // Creating a weather app where users can search the cities to see how current weather conditions.
 // Adding the jQuery and ajax call for to the javascript for the api call for the current weather, uv and five day forcast.
 //Set up the html framework for the elements to be populated from the Open weather Api.
+//Script hass all three API calls for the different elements and set up the different html elements.
--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 $("#submit").on("click", function(event){
     event.preventDefault();
     var city = $("#inputCity").val();
-    // console.log(city);
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=imperial"+ "&appid=ffa2450d91a6f2a29c9510166ab57af7"
-    // console.log(queryURL)
 
 $.ajax({
     url: queryURL,
@@ -11,7 +9,6 @@ $.ajax({
     })
     
     .then(function(response){
-        // console.log(response);
         var tBody = $("<tbody>");
         var tRow = $("<tr>");
         var tempTd = $("<td1>").text(response.main.temp);
@@ -24,11 +21,11 @@ $.ajax({
 
         td1.textContent = "Tempature:";
         td2.textContent = "Humidity";
-        td3.textContent = "Windspped";
+        td3.textContent = "Windspeed";
 
-        td1.setAttribute("style", "margin:auto; width:50%; text-align:center;");
-        td2.setAttribute("style", "margin:auto; width:50%; text-align:center;");
-        td3.setAttribute("style", "margin:auto; width:50%; text-align:center;");
+        td1.setAttribute("style", "margin:auto; width:50%; text-align:letter;");
+        td2.setAttribute("style", "margin:auto; width:50%; text-align:letter;");
+        td3.setAttribute("style", "margin:auto; width:50%; text-align:letter;");
 
 
         
@@ -40,28 +37,33 @@ $.ajax({
             method: "GET"
         })
         .then(function(uvreponse){
-            console.log(uvreponse)
             var tBody = $("<tbody>");
             var tRow = $("<tr>");
-            var valueTD = $("<td>").text(response.main.value);
+            var valueTD = $("<td4>").text(response.main.value);
             tRow.append(valueTD);
             tBody.append(tRow);
             $(".uv").append(tbody);
-            var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+ "&appid=ffa2450d91a6f2a29c9510166ab57af7"
+            td4.textContent = "UV";
+            td4.setAttribute("style","margin:auto; width:50%; text-align:letter;");
+
+            var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+ "&units=imperial"+ "&appid=ffa2450d91a6f2a29c9510166ab57af7"
             return $.ajax ({
                 url: forecastUrl,
                 method: "GET"
             })
             .then(function(forecastresponse){
-                console.log(forecastresponse);
                 var tBody = $("tbody");
                 var trow = $("<tr>");
-                var tempTd = $("<td>").text(response.main.temp);
-                var feelsTd = $("<td>").text(response.main.feels_like);
-                var humidityTd = $("<td>").text(response.main.humidity);
-                tRow.append(tempTd, feelsTd, humidityTd);
+                var tempTd = $("<td1>").text(list.main.temp);
+                var humidityTd = $("<td2>").text(list.main.humidity);
+                tRow.append(tempTd, humidityTd);
                 tbody.append(tRow);
                 $(".forecast").append(tbody);
+
+                td1.textContent = "Temp";
+                td2.textContent = "Humidity";
+                td1.setAttribute("style", "margin:auto; width:50%; text-align:letter;");
+                td2.setAttribute("style", "margin:auto; width:50%; text-align:letter;");
             });
         });
 
