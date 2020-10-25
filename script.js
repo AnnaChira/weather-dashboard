f3fed2129674ec4e35c7bf493523ce4ff3be434f
Anna Chirapongse <achirapongse@gmail.com>
2020-10-25 15:20:47 -0400

    updates to the js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $("#submit").on("click", function(event){
     event.preventDefault();
     var city = $("#inputCity").val();
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=imperial"+ "&appid=ffa2450d91a6f2a29c9510166ab57af7"
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=imperial"+ "&appid=16ab2d92a083c80457689b6401285258"
 
 $.ajax({
     url: queryURL,
@@ -13,13 +13,13 @@ $.ajax({
         var tempTd = $("<p>").text("Tempature:" + "" + response.main.temp);
         var humidityTd = $("<p>").text("Humidity" + "" + response.main.humidity);
         var windSpeedTd = $("<p>").text("Windspeed" + "" + response.wind.speed);
-        $("#temp).append(tempTd);
+        $("#temp").append(tempTd);
         $("#humidity").append(humidityTd);
         $("#windspeed").append(windSpeedTd);
 
         var lat = response.coord.lat
         var lon = response.coord.lon
-        var uvURL = "https://api.openweathermap.org/data/2.5/uvi?lat="+lat+"&lon="+lon+ "&appid=ffa2450d91a6f2a29c9510166ab57af7"
+        var uvURL = "https://api.openweathermap.org/data/2.5/uvi?lat="+lat+"&lon="+lon+ "&appid=16ab2d92a083c80457689b6401285258"
         return $.ajax({
             url: uvURL,
             method: "GET"
@@ -29,7 +29,7 @@ $.ajax({
             $("uvmain").append(uvTd);
             console.log(uvreponse);
 
-            var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+ "&units=imperial"+ "&appid=ffa2450d91a6f2a29c9510166ab57af7"
+            var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+ "&units=imperial"+ "&appid=16ab2d92a083c80457689b6401285258"
             return $.ajax ({
                 url: forecastUrl,
                 method: "GET"
@@ -43,13 +43,14 @@ $.ajax({
                 console.log(forecastresponse);
 
                 var fiveday = forecastresponse.list
-                 for (var i = 0; i< fiveday.length; i++){
+                 for (var i = 0; i< fiveday.length; i=i+8){
                     var tempTd = $("<td>");
                     var humidityTd = $("<td>");
                     tempTd.text("Tempature" + fiveday[i].main.temp);
                     humidityTd.text("Humidity" + fiveday[i].main.humidity);
                     tRow.append(tempTd, humidityTd);
                     tBody.append(tRow);
+                    console.log(fiveday[i].main.temp);
                  }
                 // tempTd.textContent = "Temp" + forecastresponse.list.main.temp;
                 // humidityTd.textContent = "Humidity" + forecastresponse.list.main.humidity;
