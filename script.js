1370a6a8db42f2c58591e888e66ad0c6f47d91a7
Anna Chirapongse <achirapongse@gmail.com>
2020-10-25 22:41:14 -0400

    Last Updates

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ $("#submit").on("click", function (event) {
     var city = $("#inputCity").val();
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&appid=ffa2450d91a6f2a29c9510166ab57af7"
 
-    city.replace(/ /g, "_");
-    localStorage.setItem('city', city);
-    $("city1").append(city)
+    // city.replace(/ /g, "_");
+    // localStorage.setItem('city', city);
+    // $("city1").append(city)
 
     $.ajax({
         url: queryURL,
@@ -13,11 +13,11 @@ $("#submit").on("click", function (event) {
     })
 
         .then(function (response) {
-            var cityTd = $("<p>").text("City:" + " " + response.name);
+            var cityTd = $("<h2>").text("City:" + " " + response.name);
             var dateTd = $("<p>").text("Date:" + " " + response.dt);
             var tempTd = $("<p>").text("Tempature:" + " " + response.main.temp + "F");
             var humidityTd = $("<p>").text("Humidity:" + " " + response.main.humidity + "%");
-            var windSpeedTd = $("<p>").text("Windspeed:" + " " + response.wind.speed + "MPH");
+            var windSpeedTd = $("<p>").text("Windspeed:" + " " + response.wind.speed + " " + "MPH");
             $("#currentdate").append(dateTd);
             $("#city").append(cityTd);
             $("#temp").append(tempTd);
@@ -32,7 +32,7 @@ $("#submit").on("click", function (event) {
                 method: "GET"
             })
                 .then(function (uvreponse) {
-                    var uvTd = $("<p>").text("UV" + uvreponse.value);
+                    var uvTd = $("<p>").text("UV:" + " " + uvreponse.value);
                     $("#uvmain").append(uvTd);
                     console.log(uvreponse);
 
@@ -53,14 +53,8 @@ $("#submit").on("click", function (event) {
                             for (var i = 0; i < fiveday.length; i = i + 8) {
                                 var fivetempTd = $("<p>").text("Tempature:" + " " + fiveday[i].main.temp + "F");
                                 var fivehumidityTd = $("<p>").text("Humidity:" + " " + fiveday[i].main.humidity + "%");
-                                // var fivedateTd = $("<p>").text("Date:" + " " + fiveday[i].list.dt);
-                                // var fiveiconTd = $("<img>").attr("src", fiveday[i].list.weather.icon);
-                                // $("date1").append(fivedateTd);
-                                // $("icon1").append(fiveiconTd);
                                 $("#temp1").append(fivetempTd);
                                 $("#humidity1").append(fivehumidityTd);
-                                $("#temp2").append(fivedateTd);
-                                $("#humidity2").append(fivehumidityTd);
                                 console.log(fiveday[i].main.temp);
                             }
                         });
